937d9dbd46771eb3ba4765c1944872d47082659c
Kevin Lackaff-Gilligan <kevin.lackaff.gilligan@gmail.com>
2017-11-01 16:19:21 -0500

    play mode static components created

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -5,6 +5,7 @@ $(document).ready(onReady);
 //globals
 var diffLevel; //will store the difficulty level picked by the users
 var max; //will hold the max number of guessing range
+var guessesMade = 0; //keeps track of guessing rounds
 
 function onReady(){
     //page has loaded
@@ -28,7 +29,7 @@ function loadSetUp(){
     $('.container').append($setUp);
 }
 
-//sets and stores difficulty level
+//sets and stores difficulty level and max guess value
 function difficultyLevelPicked(){
     diffLevel = $(this).attr('id');
     console.log('diffLevel:', diffLevel);
@@ -48,12 +49,28 @@ function startBtnClicked(){
     playMode();
   }
 
+  //create play mode of game
   function playMode(){
       var $playMode = $('<div id="playMode"></div>');
-      $playMode.append('<h2>Difficulty Level: ' + diffLevel + '</h2>');
-
-   
+      $playMode.append('<h2 id="difficultyDisp">Difficulty Level: ' + diffLevel + '</h2>');
+      $playMode.append('<h3 id="guessRange">Number is between 1 and ' + max + '</h3>');
+      $playMode.append('<h3>Guesses made: ' + guessesMade + '</h3>');
+      var playerInputs = createPlayerInputs();
+      $playMode.append(playerInputs);
+      $playMode.append('<button type="submit" id="submitBtn">Submit Guesses</button>');
+      $playMode.append('<button type="button" id="cancelBtn">Cancel Game</button>');
    $('.container').append($playMode);
+  }
+
+  //create inputs and last guess details for each player's guesses
+  function createPlayerInputs(){
+      var players = $('<div id="players"></div>');
+      for(var i = 1; i < 5; i += 1){
+       players.append('<label for="input' + i + '">Player ' + i + '</label><br>');
+       players.append('<input type="text" id="input' + i + '" class="playerInput">');
+       players.append('<p>Last Guess: <span id="lastGuess' + i + '"></span></p>');
+      }
+      return players;
   }
 
 
