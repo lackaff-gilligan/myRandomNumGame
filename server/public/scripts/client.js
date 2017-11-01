f5bff4cedaf7e3f2e15354d99bbef09f6bf72763
Kevin Lackaff-Gilligan <kevin.lackaff.gilligan@gmail.com>
2017-11-01 14:31:05 -0500

    clicking start btn now switches to play mode

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -42,15 +42,45 @@ function difficultyLevelPicked(){
 }
 
 function startBtnClicked(){
-  var objToSend = {
-      max: max,
-      diffLevel: diffLevel
-  };
-  //POST request
- $.ajax({
-     method: 'POST', //type of request
-     url: '/number', //route that I will match on
-     data: objToSend
- })
-}
+    //clear the container div of loadSetUp()
+    $('.container').empty();
+    //call function that appends play mode to the DOM
+    playMode();
+  }
 
+  function playMode(){
+      var $playMode = $('<div id="playMode"></div>');
+      $playMode.append('<h2>Difficulty Level: ' + diffLevel + '</h2>');
+
+   
+   $('.container').append($playMode);
+  }
+
+
+  //POST request
+//  $.ajax({
+//      method: 'POST', //type of request
+//      url: '/number', //route that I will match on (needs to be same in server.js)
+//      data: objToSend
+//  }).done(function(response){
+//      console.log('response from POST request', response);
+//      //normally would call function that makes GET request here...
+//  }).fail(function(error){
+//      console.log('something went wrong in POST request', error);
+     
+//  });
+
+
+//GET request
+// $.ajax({
+//     method: 'GET',
+//     url: '/number'
+// }).done(function(response){
+//     console.log('SUCCESS');
+//     console.log(response);
+//     //append products to the DOM???
+    
+// }).fail(function(error){
+//     console.log('something went wrong in GET request', error);
+    
+// });
--- server/routes/number_router.js
+++ server/routes/number_router.js
@@ -3,6 +3,13 @@ var router = express.Router();
 //randomNumber now holds whatever the module is set to share 
 //(in this case it's an object)
 var randomNumber = require('../modules/random_number.js'); 
+//will hold maximum number in guessing range
+var max;
 
+router.post('/', function(req, res){
+    var max = req.body;
+    console.log('req.body now max:', max);
+    res.sendStatus(201);
+})
 
 module.exports = router;
